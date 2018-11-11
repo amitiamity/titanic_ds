95972bf1be6f6fd2d037678c0dadd06307ff021c
amitiamity <amitptl.in@gmail.com>
2018-11-11 10:58:34 +0530

    indexing, filtering and slicing the data

--- src/practice/exploring_processing_data.py
+++ src/practice/exploring_processing_data.py
@@ -20,6 +20,33 @@ df = pd.concat((train_df,test_df),axis=0)
 #axis = 0, append the data of second df into first one
 #axis = 1, append the second df next to the first df columns, default is axis  = 0
 # print(df.info())
-print(df.tail())
+# print(df.tail())
+
+#selection, indexes, filtering using pandas
+#1. selecting specific columns
+# print(df.Name)
+# print(df['Name'])
+#2. multiple cols
+# print(df[['Name','Age']])
+#3. label based indexing #all columns, fetch rows between 5 to 10
+# print(df.loc[5:10,])
+
+#4 selecting column range
+# print(df.loc[5:10,'Age':'Ticket'])
+#5 selecting discrete columns
+print(df.loc[5:10,['Age','Ticket']])
+
+#position based indexing
+print(df.info())
+print(df.iloc[5:10,3:5])
+
+#filtering data
+print(df.loc[df.Sex == 'male',['Age']])
+#all columns and rows where Sex is male
+print(df.loc[df.Sex== 'male',:])
+#use & and or operator
+print(df.loc[((df.Sex == 'male') & (df.Pclass == 1)),:])
+
+#Summary Statistics
 
 
